56b1d9616c856d36c533214c9170ac05f41330af
lushl9301 <lushl9301@gmail.com>
2013-09-22 23:10:06 +0800

    how to import other module--javascript if style

--- learnyouahaskell/Equal.hs
+++ learnyouahaskell/Equal.hs
@@ -1,18 +1,19 @@
-import Prelude hiding ((==))
-(==) :: (Eq a) => a -> a -> Bool
+--import Prelude hiding ((==))
+--(==) :: (Eq a) => a -> a -> Bool
+module Equal where
 data TrafficLight = Red | Yellow | Green
-
-class Eq a where 
-  (==), (/=) :: a -> a -> Bool
-  x == y = not (x /= y)
-  x /= y = not (x == y)
+  
+--class Eq a where 
+--  (==), (/=) :: a -> a -> Bool
+--  x == y = not (x /= y)
+--  x /= y = not (x == y)
 
 instance Eq TrafficLight where
   Red == Red = True
   Green == Green = True
   Yellow == Yellow = True
   _ == _ = False
-  
+           
 instance Show TrafficLight where
   show Red = "Red light"
   show Yellow = "Yello light"
---
+++ learnyouahaskell/jsStyle.hs
@@ -0,0 +1,31 @@
+import Equal
+import Tree
+class YesNo a where
+  yesno :: a -> Bool
+           
+instance YesNo Int where
+  yesno 0 = False
+  yesno _ = True
+            
+instance YesNo [a] where
+  yesno [] = False
+  yesno _ = True
+  
+instance YesNo Bool where
+  yesno = id
+
+instance YesNo (Maybe a) where
+  yesno (Just _) = True
+  yesno Nothing = False
+  
+instance YesNo TrafficLight where
+  yesno Red = False
+  yesno _ = True
+  
+instance YesNo (Tree a) where
+  yesno EmptyTree = False
+  yesno _ = True
+  
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult = 
+  if (yesno yesnoVal) then yesResult else noResult 
